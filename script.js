48ea627eaeac69baf2fe5bc166f7ddbe70c9dd76
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-06-23 13:35:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,101 @@
 import { useState } from "https://cdn.skypack.dev/react@17.0.1";
+
 function App() {
   const [team, setTeam] = React.useState([]);
 
   const fetchData = () => {
-    return fetch("https://Darlingson.pythonanywhere.com/stats").
-    then(response => response.json()).
-    then(data => setTeam(data));
+    return fetch("https://Darlingson.pythonanywhere.com/stats")
+      .then((response) => response.json())
+      .then((data) => {
+        setTeam(data);
+      });
   };
 
   React.useEffect(() => {
     fetchData();
   }, []);
 
-  return /*#__PURE__*/(
-    React.createElement("main", null, /*#__PURE__*/
-    React.createElement("h1", null, "Team List"), /*#__PURE__*/
-    React.createElement("ul", null,
-    team && team.length > 0 && team.map((teamObj, index) => /*#__PURE__*/
-    React.createElement("li", { key: teamObj.match_id }, teamObj.Home)))));
+  return (
+    <main>
+      <h1>Team List</h1>
+      <ul>
+        {/*team &&
+          team.length > 0 &&
+          team.map((teamObj, index) => (
+            <li key={teamObj.match_id}>{teamObj.Home}</li>
+          ))*/}
 
+        {team &&
+          team.length > 0 &&
+          team.map((teamObj, index) => (
+            <Team name={teamObj.Home} />
+            //<li key={teamObj.match_id}>{teamObj.Home}</li>
+          ))}
+      </ul>
+    </main>
+  );
+}
+function Team(props) {
+  return (
+    <>
+      <div className="row">
+        <div className="col s12 m7">
+          <div className="card-panel teal">
+            <span className="white-text">{props.name}</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
+function GoalsChart() {
+  const [homegoalData, sethomegoalData] = React.useState([]);
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = () => {
+    return fetch("https://Darlingson.pythonanywhere.com/stats")
+      .then((response) => response.json())
+      .then((data) => {
+        const avgGoals = [];
+        const homeGoals = [];
+        const awayGoals = [];
 
+        for (let i = 0; i < data.length; i++) {
+          homeGoals.push(data[i].Home_Goal);
+          awayGoals.push(data[i].Away_Goal);
+        }
 
+        const TotalGoalsPerGame = data.map(
+          (game, index) => game.Home_Goal + game.Away_Goal
+        );
+        const avgGPG =
+          TotalGoalsPerGame.reduce((x, y) => x + y) / TotalGoalsPerGame.length;
+        const avgHG = homeGoals.reduce((x, y) => x + y) / homeGoals.length;
+        const avgAG = awayGoals.reduce((x, y) => x + y) / awayGoals.length;
+        avgGoals.push(avgGPG);
+        avgGoals.push(avgHG);
+        avgGoals.push(avgAG);
+        sethomegoalData(avgGoals);
+      });
+  };
+  return (
+    <>
+      <div>hi</div>
+      <div>{homegoalData}</div>
+    </>
+  );
+}
+function RootApp() {
+  return (
+    <>
+      <App />
+      <GoalsChart />
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-let rootElement = /*#__PURE__*/React.createElement(App, null);
+let rootElement = <RootApp />;
 root.render(rootElement);
