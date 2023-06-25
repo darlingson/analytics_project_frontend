9804c8228eff9786a3f430742bb0d3b3159c93d3
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-06-25 10:22:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ import { useState } from "https://cdn.skypack.dev/react@17.0.1";
 
 function App() {
   const [team, setTeam] = React.useState([]);
+  const [showButton, setShowButton] = React.useState(false);
 
+  const toggleButton = () => {
+    setShowButton(!showButton);
+  };
   const fetchData = () => {
     return fetch("https://Darlingson.pythonanywhere.com/stats").
     then(response => response.json()).
@@ -16,21 +20,17 @@ function App() {
   }, []);
 
   return /*#__PURE__*/(
-    React.createElement("main", null, /*#__PURE__*/
+    React.createElement("main", { className: "hidenTeams" }, /*#__PURE__*/
     React.createElement("h1", null, "Team List"), /*#__PURE__*/
-    React.createElement("ul", null,
+    React.createElement("button", { className: "btn", onClick: toggleButton }, "Show/Hide Teams"),
 
 
-
-
-
-
+    showButton && /*#__PURE__*/
+    React.createElement("div", null,
     team &&
     team.length > 0 &&
-    team.map((teamObj, index) => /*#__PURE__*/
-    React.createElement(Team, { name: teamObj.Home })
-    //<li key={teamObj.match_id}>{teamObj.Home}</li>
-    ))));
+    team.map((teamObj, index) => /*#__PURE__*/React.createElement(Team, { name: teamObj.Home })))));
+
 
 
 
@@ -39,7 +39,7 @@ function Team(props) {
   return /*#__PURE__*/(
     React.createElement(React.Fragment, null, /*#__PURE__*/
     React.createElement("div", { className: "row" }, /*#__PURE__*/
-    React.createElement("div", { className: "col s12 m7" }, /*#__PURE__*/
+    React.createElement("div", { className: "col s12 m12" }, /*#__PURE__*/
     React.createElement("div", { className: "card-panel teal" }, /*#__PURE__*/
     React.createElement("span", { className: "white-text" }, props.name))))));
 
@@ -48,7 +48,16 @@ function Team(props) {
 
 
 }
+function Stat(prop) {
+  return /*#__PURE__*/(
+    React.createElement(React.Fragment, null, /*#__PURE__*/
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "col s4" }, prop.statName), /*#__PURE__*/
+    React.createElement("div", { className: "col s4" }, prop.statFig))));
 
+
+
+}
 function GoalsChart() {
   const [homegoalData, sethomegoalData] = React.useState([]);
   React.useEffect(() => {
@@ -74,6 +83,8 @@ function GoalsChart() {
       TotalGoalsPerGame.reduce((x, y) => x + y) / TotalGoalsPerGame.length;
       const avgHG = homeGoals.reduce((x, y) => x + y) / homeGoals.length;
       const avgAG = awayGoals.reduce((x, y) => x + y) / awayGoals.length;
+
+      avgGoals.push(TotalGoalsPerGame.reduce((x, y) => x + y));
       avgGoals.push(avgGPG);
       avgGoals.push(avgHG);
       avgGoals.push(avgAG);
@@ -82,8 +93,24 @@ function GoalsChart() {
   };
   return /*#__PURE__*/(
     React.createElement(React.Fragment, null, /*#__PURE__*/
-    React.createElement("div", null, "hi"), /*#__PURE__*/
-    React.createElement("div", null, homegoalData)));
+    React.createElement("h1", null, "Goal Stats"), ">",
+    homegoalData &&
+    homegoalData.length > 0 &&
+    homegoalData.map((statObj, index) => console.log(statObj)),
+    homegoalData && homegoalData.length > 0 && /*#__PURE__*/
+    React.createElement(React.Fragment, null, /*#__PURE__*/
+    React.createElement(Stat, { statName: "Total Goals Scored", statFig: homegoalData[0] }), /*#__PURE__*/
+    React.createElement(Stat, { statName: "Average Goals Per Game", statFig: homegoalData[1] }), /*#__PURE__*/
+    React.createElement(Stat, {
+      statName: "Average Goals for Home Team",
+      statFig: homegoalData[2] }), /*#__PURE__*/
+
+    React.createElement(Stat, {
+      statName: "Average Goals for Away Team",
+      statFig: homegoalData[3] }))));
+
+
+
 
 
 }
